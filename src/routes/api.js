57bc5b0fb343bd90ba78efecf126b47e9197af1d
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-07-31 20:43:09 -0400

    feat: move selection box if its out of bounds

--- src/routes/api.js
+++ src/routes/api.js
@@ -37,5 +37,37 @@ router.post("/waldo", async (req, res, next) => {
     res.json({ result: "Wrong!" });
   }
 });
+router.post("/odlaw", async (req, res, next) => {
+  let record = await req.context.models.Temp.findById(req.body.id);
+  if (
+    req.body.x >= 310 &&
+    req.body.x <= 330 &&
+    req.body.y >= 820 &&
+    req.body.y <= 900
+  ) {
+    record = await req.context.models.Temp.findByIdAndUpdate(req.body.id, {
+      odlaw: true,
+    });
+    res.json({ result: "Right!" });
+  } else {
+    res.json({ result: "Wrong!" });
+  }
+});
+router.post("/wizard", async (req, res, next) => {
+  let record = await req.context.models.Temp.findById(req.body.id);
+  if (
+    req.body.x >= 795 &&
+    req.body.x <= 845 &&
+    req.body.y >= 815 &&
+    req.body.y <= 880
+  ) {
+    record = await req.context.models.Temp.findByIdAndUpdate(req.body.id, {
+      whitebeard: true,
+    });
+    res.json({ result: "Right!" });
+  } else {
+    res.json({ result: "Wrong!" });
+  }
+});
 
 export default router;
